cf3794b8b27b9ed1bc3aa9d15703333d601b0988
AnjaliSHP <anjalijagtap.shp@gmail.com>
2025-02-12 19:56:33 +0530

    add comma-Separated Numbers.

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,18 @@ import 'package:flutter/foundation.dart';
 void main() {
   if (kDebugMode) {
     print(add(''));
+    print(add('1'));
+    print(add('1,1'));
   }
 }
 
 int add(String numbers) {
-  ///Handle Empty String
+  //Handle Empty String
   if (numbers.isEmpty) {
     return 0;
   }
-  return int.parse(numbers);  // convert string to int
+  //Handle Comma-Separated Numbers
+  List<String> numberList = numbers.split(',');
+  //add all numbers of numberList
+  return numberList.fold(0, (sum, num) => sum + int.parse(num));
 }
