08927a68573cb690a8e357655c12860d9d7bf2ce
AnjaliSHP <anjalijagtap.shp@gmail.com>
2025-02-12 20:01:02 +0530

    handle custom delimiters

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() {
     print(add(''));
     print(add('1'));
     print(add('1,1'));
+    print(add('1\n22,4'));
+    print(add('//;\n1;7'));
   }
 }
 
@@ -13,10 +15,20 @@ int add(String numbers) {
   if (numbers.isEmpty) {
     return 0;
   }
-  // Replace new lines with commas
-  numbers = numbers.replaceAll('\n', ',');
+  // Check if there is a custom delimiter
+  if (numbers.startsWith('//')) {
+    int delimiterEnd = numbers.indexOf('\n');
+    String delimiter = numbers.substring(2, delimiterEnd);
+    numbers = numbers.substring(delimiterEnd + 1);
+
+    // Replace custom delimiter with commas
+    numbers = numbers.replaceAll(delimiter, ',');
+  } else {
+    numbers = numbers.replaceAll('\n', ','); // Replace newlines with commas
+  }
+
   //Handle Comma-Separated Numbers
   List<String> numberList = numbers.split(',');
   //add all numbers of numberList
-  return numberList.fold(0, (sum, num) => sum + int.parse(num));
+  return numberList.fold(0, (sum, number) => sum + int.parse(number));
 }
