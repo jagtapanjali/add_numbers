a921851cf3a1d8c1e2adcd2b83221aa99b1948f7
AnjaliSHP <anjalijagtap.shp@gmail.com>
2025-02-12 19:30:01 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+void main() {
+  if (kDebugMode) {
+    print(add(''));
+  }
+}
+
+int add(String numbers) {
+  ///Handle Empty String
+  if (numbers.isEmpty) {
+    return 0;
+  }
+  return 0;  // placeholder
+}
